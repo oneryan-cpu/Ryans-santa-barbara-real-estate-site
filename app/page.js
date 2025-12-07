89fa4937e9d28f64f20ee8b4c71046890c619684
oneryan-cpu <oneryan@gmail.com>
2025-12-06 18:55:07 -0800

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Ryan’s Santa Barbara Real Estate</h1>
-      <p>Your site is successfully deployed!</p>
-    </div>
-  );
-}
