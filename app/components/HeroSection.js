693f5a8d3b42d82332ff853409e22ddbd7eb45cd
oneryan-cpu <oneryan@gmail.com>
2025-12-07 09:57:58 -0800

    Update HeroSection.js

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -1,1 +1,17 @@
+"use client";
+
+export default function HeroSection({ currentImage }) {
+  return (
+    <section
+      style={{
+        width: "100%",
+        height: "80vh",
+        backgroundImage: `url(${currentImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        transition: "background-image 1s ease-in-out",
+      }}
+    />
+  );
+}
 
