0cd2b9565e3b33bc0ccff05644c2a4f3fd2a9f07
oneryan-cpu <oneryan@gmail.com>
2025-12-07 10:03:50 -0800

    Create FeaturedProperties.js

---
+++ app/components/FeaturedProperties.js
@@ -0,0 +1,14 @@
+"use client";
+
+export default function FeaturedProperties() {
+  return (
+    <section style={{ padding: "40px", textAlign: "center" }}>
+      <h2>Featured Properties</h2>
+      <ul>
+        <li>Property 1</li>
+        <li>Property 2</li>
+        <li>Property 3</li>
+      </ul>
+    </section>
+  );
+}
