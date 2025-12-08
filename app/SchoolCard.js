14506cae8680d5f0b58b74671ef378daac645e2e
oneryan-cpu <oneryan@gmail.com>
2025-12-08 11:28:11 -0800

    Update SchoolCard.js

--- app/SchoolCard.js
+++ app/SchoolCard.js
@@ -1,20 +1,21 @@
 'use client';
 
-export default function SchoolCard({ name, image, url }) {
-  return (
-    <a href={url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
-      <div style={{
-        border: '1px solid #eee',
-        borderRadius: 8,
-        overflow: 'hidden',
-        boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
-        backgroundColor: '#fff'
-      }}>
-        <img src={image} alt={name} style={{ width: '100%', height: 200, objectFit: 'cover' }} />
-        <div style={{ padding: '1rem' }}>
-          <h3 style={{ fontSize: '1.25rem', fontWeight: 700, marginBottom: '0.5rem', color: '#0070f3' }}>{name}</h3>
-        </div>
-      </div>
-    </a>
-  );
-}
+import SchoolCard from '../components/SchoolCard';
+
+export default function Schools() {
+  const schools = [
+    { name: 'La Colina School', image: '/schools/la-colina.jpg', url: 'https://lacolina.sbunified.org/' },
+    { name: 'La Cumbre Junior High', image: '/schools/la-cumbre.jpg', url: 'https://lacumbre.sbunified.org/' },
+    { name: 'Goleta Valley Junior High', image: '/schools/gvjh.jpg', url: 'https://gvjh.sbunified.org/' },
+    { name: 'Santa Barbara Middle School', image: '/schools/sbms.jpg', url: 'https://www.sbms.org/' },
+    { name: 'Adams Elementary', image: '/schools/adams.jpg', url: 'https://adams.sbunified.org/' },
+    { name: 'Monroe Elementary', image: '/schools/monroe.jpg', url: 'https://monroe.sbunified.org/' },
+    { name: 'Roosevelt Elementary', image: '/schools/roosevelt.jpg', url: 'https://roosevelt.sbunified.org/' },
+    { name: 'Peabody Charter', image: '/schools/peabody.jpg', url: 'https://www.peabodycharter.org/' },
+    { name: 'Washington Elementary', image: '/schools/washington.jpg', url: 'https://washington.sbunified.org/' },
+    { name: 'Cleveland Elementary', image: '/schools/cleveland.jpg', url: 'https://cleveland.sbunified.org/' },
+    { name: 'Harding Elementary', image: '/schools/harding.jpg', url: 'https://harding.sbunified.org/' },
+    { name: 'Franklin Elementary', image: '/schools/franklin.jpg', url: 'https://franklin.sbunified.org/' },
+    { name: 'McKinley Elementary', image: '/schools/mckinley.jpg', url: 'https://mckinley.sbunified.org/' },
+    { name: 'Cold Spring School', image: '/schools/coldspring.jpg', url: 'https://www.coldspringschool.net/' },
+    { name: 'Mount Carmel School', image: '/schools/mountcarmel.jpg', url:
