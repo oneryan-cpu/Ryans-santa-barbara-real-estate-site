9d9d70092e45271a57097c5b37cb14bd2ab493b8
oneryan-cpu <oneryan@gmail.com>
2025-12-08 11:15:11 -0800

    Update schools.js

--- app/schools.js
+++ app/schools.js
@@ -1,35 +1,38 @@
 'use client';
 
-import SchoolCard from '../../components/SchoolCard';
+import SchoolCard from './components/SchoolCard';
 
-// Deployment-safe placeholder images
-// Replace these with the real school images later
 const schools = [
-  { name: 'Santa Barbara High', image: '/placeholder1.jpg', description: 'Top public high school in Santa Barbara.', url: 'https://sbhighschool.com' },
-  { name: 'Montecito Union School', image: '/placeholder2.jpg', description: 'Elementary & middle school with strong academics.', url: 'https://montecitounion.org' },
-  { name: 'Carpinteria Middle School', image: '/placeholder3.jpg', description: 'Highly rated school with a great community.', url: 'https://cms.cusd.net/' },
-  { name: 'La Colina Elementary', image: '/placeholder1.jpg', description: 'Elementary school in Santa Barbara.', url: 'https://lacolina.sbunified.org/' },
-  { name: 'La Cumbre Elementary', image: '/placeholder2.jpg', description: 'Elementary school in Santa Barbara.', url: 'https://lacumbre.sbunified.org/' },
-  { name: 'Goleta Valley Junior High', image: '/placeholder3.jpg', description: 'Middle school serving Goleta.', url: 'https://gvjh.sbunified.org/' },
-  { name: 'Santa Barbara Middle School', image: '/placeholder1.jpg', description: 'Middle school in Santa Barbara.', url: 'https://www.sbms.org/' },
-  { name: 'Riviera School', image: '/placeholder2.jpg', description: 'Elementary school with a community focus.', url: 'https://www.rivieraschool.org/' },
-  { name: 'Howard School', image: '/placeholder3.jpg', description: 'Private school in Carpinteria.', url: 'https://www.thehowardschool.org/' },
-  { name: 'Cate School', image: '/placeholder1.jpg', description: 'Private college preparatory school.', url: 'https://www.cate.org/' },
-  { name: 'Westmont College', image: '/placeholder2.jpg', description: 'Private liberal arts college.', url: 'https://www.westmont.edu/' },
-  { name: 'Pacifica Graduate Institute', image: '/placeholder3.jpg', description: 'Graduate programs in depth psychology.', url: 'https://www.pacifica.edu/' },
+  { name: 'La Colina Elementary', image: '/schools/lacolina.jpg', url: 'https://lacolina.sbunified.org/' },
+  { name: 'La Cumbre Junior High', image: '/schools/lacumbre.jpg', url: 'https://lacumbre.sbunified.org/' },
+  { name: 'Goleta Valley Junior High', image: '/schools/gvjhs.jpg', url: 'https://gvjh.sbunified.org/' },
+  { name: 'Santa Barbara Middle School', image: '/schools/sbms.jpg', url: 'https://www.sbms.org/' },
+  { name: 'Adams Elementary', image: '/schools/adams.jpg', url: 'https://adams.sbunified.org/' },
+  { name: 'Monroe Elementary', image: '/schools/monroe.jpg', url: 'https://monroe.sbunified.org/' },
+  { name: 'Roosevelt Elementary', image: '/schools/roosevelt.jpg', url: 'https://roosevelt.sbunified.org/' },
+  { name: 'Peabody Charter', image: '/schools/peabody.jpg', url: 'https://www.peabodycharter.org/' },
+  { name: 'Washington Elementary', image: '/schools/washington.jpg', url: 'https://washington.sbunified.org/' },
+  { name: 'Cleveland Elementary', image: '/schools/cleveland.jpg', url: 'https://cleveland.sbunified.org/' },
+  { name: 'Harding Elementary', image: '/schools/harding.jpg', url: 'https://harding.sbunified.org/' },
+  { name: 'Franklin Elementary', image: '/schools/franklin.jpg', url: 'https://franklin.sbunified.org/' },
+  { name: 'McKinley Elementary', image: '/schools/mckinley.jpg', url: 'https://mckinley.sbunified.org/' },
+  { name: 'Cold Spring School', image: '/schools/coldspring.jpg', url: 'https://www.coldspringschool.net/' },
+  { name: 'Mount Carmel School', image: '/schools/mountcarmel.jpg', url: 'https://mountcarmelschool.net/' },
+  { name: 'Carpinteria Middle', image: '/schools/cms.jpg', url: 'https://cms.cusd.net/' },
+  { name: 'Aliso Elementary', image: '/schools/aliso.jpg', url: 'https://aliso.cusd.net/' },
+  { name: 'Canalino Elementary', image: '/schools/canalino.jpg', url: 'https://canalino.cusd.net/' },
+  { name: 'Summerland School', image: '/schools/summerland.jpg', url: 'https://summerland.cusd.net/' },
+  { name: 'The Howard School', image: '/schools/howard.jpg', url: 'https://www.thehowardschool.org/' },
+  { name: 'Cate School', image: '/schools/cate.jpg', url: 'https://www.cate.org/' },
+  { name: 'Pacifica Graduate Institute', image: '/schools/pacifica.jpg', url: 'https://www.pacifica.edu/' },
+  { name: 'Westmont College', image: '/schools/westmont.jpg', url: 'https://www.westmont.edu/' },
 ];
 
-export default function SchoolsPage() {
+export default function Schools() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
       {schools.map((s) => (
-        <SchoolCard
-          key={s.name}
-          name={s.name}
-          description={s.description}
-          image={s.image}
-          url={s.url}
-        />
+        <SchoolCard key={s.name} name={s.name} image={s.image} url={s.url} />
       ))}
     </div>
   );
